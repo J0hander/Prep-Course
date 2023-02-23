cafe148d85b3679311f6ff600e6f279e568f388c
J0hander <johandercedeno@gmail.com>
2023-02-22 22:59:49 -0300

    Progreso repositorio 22 Feb

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -115,6 +115,10 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero % 1 === 0) {
+    return true;
+  }
+    return false;
 }
 
 function fizzBuzz(numero) {
@@ -192,19 +196,34 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  let arrayTabladelseis =[];
+  for (let i = 0; i < 11; i++) {
+    arrayTabladelseis.push(6 * i);
+  }
+  return arrayTabladelseis;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero >= 100 && numero <= 999  ) {
+    return true;
+  } 
+    return false;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var a = numero;
+  var i = 0;
+  do {
+    a = a + 5;
+    i = i + 1;
+  } while (i < 8) 
+
+  return a;
 }
 
 
